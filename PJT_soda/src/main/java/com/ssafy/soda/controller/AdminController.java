0ed2722aab551d0bdb61998c31c7ddc230182028
Lee DaYoung <daosp1023@gmail.com>
2024-11-04 22:52:49 +0900

    241104

--- PJT_soda/src/main/java/com/ssafy/soda/controller/AdminController.java
+++ PJT_soda/src/main/java/com/ssafy/soda/controller/AdminController.java
@@ -1,17 +1,64 @@
 package com.ssafy.soda.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ssafy.soda.model.dto.User;
+import com.ssafy.soda.model.service.AdminUserService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 	
+	//서비스 싱글턴 주입
+	private AdminUserService adminUserService;
+
+	public AdminController(AdminUserService adminUserService) {
+		super();
+		this.adminUserService = adminUserService;
+	}
+	//메인 화면 이동
 	@GetMapping("/main")
 	public String adminMain() {
 	System.out.println("확인");
 		return "admin/adminMain";
 	}
+	
+	//사용자 전체목록 이동
+	@GetMapping("/adminUserBoard")
+	public String list(Model model) {
+		List<User> users = adminUserService.getAllUser();
+		model.addAttribute("users", users);
+		return "adminUserBoard";
+	}
+	
+	//사용자 상세보기
+	@GetMapping("/adminUserDetail")
+	public String detail(@RequestParam("id")int id, Model model) {
+		return "adminUserDetail";
+		
+	}
+	
+	//사용자 삭제
+	@GetMapping("/adminUserBoard")
+	public String delete(@RequestParam("id") int id) {
+		return "adminUserBoard";
+	}
+	
+	//로그인하기
+	
+	//신고된 사용자 조회
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- PJT_soda/src/main/java/com/ssafy/soda/model/dto/User.java
+++ PJT_soda/src/main/java/com/ssafy/soda/model/dto/User.java
@@ -1,5 +1,125 @@
 package com.ssafy.soda.model.dto;
 
+import java.time.LocalDate;
+
 public class User {
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+	private String phoneNumber;
+	private LocalDate birthDate;
+	private String gender;
+	private String address;
+	private String detail_address;
+	private String zonecode;
+	
+	public User() {
+	}
+
+	public User(String userId, String password, String name, String email, String phoneNumber, LocalDate birthDate,
+			String gender, String address, String detail_address, String zonecode) {
+		super();
+		this.userId = userId;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.birthDate = birthDate;
+		this.gender = gender;
+		this.address = address;
+		this.detail_address = detail_address;
+		this.zonecode = zonecode;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDetail_address() {
+		return detail_address;
+	}
+
+	public void setDetail_address(String detail_address) {
+		this.detail_address = detail_address;
+	}
+
+	public String getZonecode() {
+		return zonecode;
+	}
+
+	public void setZonecode(String zonecode) {
+		this.zonecode = zonecode;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email
+				+ ", phoneNumber=" + phoneNumber + ", birthDate=" + birthDate + ", gender=" + gender + ", address="
+				+ address + ", detail_address=" + detail_address + ", zonecode=" + zonecode + "]";
+	}
+	
+	
+	
 	
 }

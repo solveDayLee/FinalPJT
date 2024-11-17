10bdf143f844ac60e4461c526ddb7e3ef3912a1b
Lee DaYoung <daosp1023@gmail.com>
2024-11-17 17:16:30 +0900

    1

--- PJT_soda_vue/soda-vue/src/App.vue
+++ PJT_soda_vue/soda-vue/src/App.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>
-    <h2>App vue</h2>
-  </div>
+  <header>
+    <div class="header-container">
+      <img :src="logo" alt="엣코 로고" class="logo-container">
+
+      <nav class="nav-menu">
+        <div>Home</div>
+        <div>Category</div>
+        <div>About</div>
+        <div>Contact</div>
+
+      </nav>
+      <div class="search-container">
+        <input type="text">
+
+      </div>
+
+
+
+    </div>
+
+  </header>
 </template>
 
 <script setup>
-
+import logo from '@/assets/엣코 로고.gif'
 </script>
 
 <style scoped>
+header {
+  padding: 1rem;
+  background-color: rgb(178, 242, 165);
+  height: auto;
+}
 
+.header-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-menu {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4rem;
+}
+
+
+
+.logo-container {
+  max-width: 80px;
+  height: auto;
+  background-color: black;
+}
 </style>

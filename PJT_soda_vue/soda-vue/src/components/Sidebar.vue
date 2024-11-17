9652637536f125a4e0bc3c7f7d6a621485046e6a
soun <sozzykim@naver.com>
2024-11-17 21:30:46 +0900

    1117 vue

---
+++ PJT_soda_vue/soda-vue/src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>전체글</th>
+            </tr>
+            <hr>
+            
+        </thead>
+
+    </table>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>

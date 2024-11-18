40932dda33d48364bb7f577665ff80aa5f329e93
DayLee <daosp1023@gmail.com>
2024-11-18 16:00:42 +0900

    1118

--- PJT_soda_vue/soda-vue/src/App.vue
+++ PJT_soda_vue/soda-vue/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <Header />
   <Banner />
+  <Sidebar />
+  <Footer />
 </template>
 
 <script setup>
 import Header from '@/components/Header.vue'
 import Banner from '@/components/Banner.vue'
+import Sidebar from '@/components/Sidebar.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <style scoped>
 
-</style>
+</style> 

26c0a9869ec44bdf25ade367fa8df6878d3510e3
DayLee <daosp1023@gmail.com>
2024-11-19 17:56:16 +0900

    1119

--- PJT_soda_vue/soda-vue/src/router/index.js
+++ PJT_soda_vue/soda-vue/src/router/index.js
@@ -2,6 +2,8 @@ import BoardView from '@/views/BoardView.vue'
 import JoinView from '@/views/JoinView.vue'
 import WriteView from '@/views/WriteView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import DetailView from '@/views/DetailView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
